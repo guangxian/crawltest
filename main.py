eee7872cac5b6f8cc7fdf2f9ded4027d49999079
phz <89360944@qq.com>
2025-10-20 14:04:31 +0800

    richang

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import time
 import json
 from datetime import datetime, timedelta
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 class Mx:
     def __init__(self):
@@ -75,15 +77,23 @@ class Mx:
             "User-Agent": "Mozilla/5.0",
             "Authorization": auth
         }
-        response = requests.post(
-            url=url,
-            json=data,
-            headers=headers,
-            timeout=300
-        )
-        response.raise_for_status()
-        response_data = response.json()
-        print("request result: ", response_data)
+        session = requests.Session()
+        retries = Retry(total=5, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504],
+                        allowed_methods=["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"])
+        adapter = HTTPAdapter(max_retries=retries)
+        session.mount("https://", adapter)
+        session.mount("http://", adapter)
+
+        try:
+            resp = session.post(
+                url="https://open.feddon.com/api/edq/stage/create_stage",
+                headers=headers,
+                json=data,
+                timeout=60)  # 增加 timeout
+            resp.raise_for_status()
+            print(resp.text)
+        except requests.exceptions.RequestException as e:
+            print("请求失败：", e)
 
     def go(self):
         now = datetime.now()
