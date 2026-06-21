b8e9cfeacfe3e1590cc8c6bd519d9758d7e6b3a9
phz <89360944@qq.com>
2026-06-21 19:32:19 +0800

    richang

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import time
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+import random
 
 class Mx:
     def __init__(self):
@@ -94,21 +95,22 @@ class Mx:
         print("request time: ", now.strftime("%Y-%m-%d %H:%M:%S"))
         self.get_data()
 
+    def start(self):
+        self.check_crawl_no()
+
     def general_url(self):
         return ('https://open.fedd' +
                 'on.com/a' +
                 'pi/edq/')
-        pass
 
     def general_key(self):
         return ('yH5l9Mx9V4NZg' +
                 'JWV5NDI4rf' +
                 'WbmCUPsnh'
                 )
-        pass
 
     def check_crawl_no(self):
-        print('check_crawl_no')
+        print('check_crawl_no 检查采集号是否已存在')
         url = self.general_url() + "stage/has_crawl_no"
         payload = {
             "crawlNo": datetime.now().strftime("%Y-%m-%d"),
@@ -137,13 +139,74 @@ class Mx:
             print("响应内容：", response.text)
 
     def start_crawl(self):
-        print('start_crawl')
-        self.get_data()
+        print('start_crawl 开始采集')
+        api_url1 = "http://xxfb.mw"
+        api_url2 = "r.cn/hydroSe"
+        api_url3 = "arch/mapSearch"
+        url = api_url1 + api_url2 + api_url3
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0"
+        }
+        payload = {
+            "name": '周堂桥'  # 可根据需求动态生成
+        }
+
+        response = requests.post(url, json=payload, headers=headers, timeout=100)
+        if response.status_code == 200:
+            print("请求成功")
+            data = response.json()
+            result = data.get('result', [])
+            print(f'result长度: {len(result)}')
+            if len(result) > 0:
+                checked = self.check_is_target_date(result)
+                if checked:
+                    self.insert_data(result)
+            else:
+                print(f'结束任务')
+        else:
+            print(f"请求失败，状态码：{response.status_code}")
+            print("响应内容：", response.text)
+
+    def check_is_target_date(self, items):
+        print('check_is_target_date 检查采集的数据中的日期是否为指定日期')
+        checked = False
+        if len(items) > 0:
+            random_numbers = [random.randint(0, len(items)) for _ in range(3)]
+            for number in random_numbers:
+                checked = self.is_today(items[number]['createTime'])
+                if not checked:
+                    print(f'随机抽查成员的创建日期是否为今日，不合格项 stnm: {items[number]['stnm']}, createTime: {items[number]['createTime']}')
+                    break
+        return checked
+
+    def insert_data(self, items):
+        print('insert_data 插入数据')
+        data = {
+            'result': items
+        }
+        self.format_data(data)
+
+    def is_today(self, target_time_str):
+        # target_time_str = '2026-06-21 10:20:28'
+
+        # 1. 定义固定的东八区 (UTC+8)
+        beijing_tz = timezone(timedelta(hours=8))
+
+        # 2. 解析字符串并赋予北京时区
+        target_time = datetime.strptime(target_time_str, '%Y-%m-%d %H:%M:%S').replace(tzinfo=beijing_tz)
+
+        # 3. 获取当前的北京时间
+        now_beijing = datetime.now(beijing_tz)
+
+        # 4. 比较日期部分
+        is_today = target_time.date() == now_beijing.date()
+
+        # print(f"当前北京时间: {now_beijing.strftime('%Y-%m-%d %H:%M:%S')}")
+        # print(f"是否为今天: {is_today}")
+        return is_today
 
 if __name__ == '__main__':
     m = Mx()
-    # m.go()
-
-
-    m.check_crawl_no()
-    # print(datetime.now().strftime("%Y-%m-%d"))
+    # m.check_crawl_no()
+    m.start()
